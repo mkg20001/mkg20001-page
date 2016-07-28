a21735323d0109ac10c7df3a439a11a4718dac02
mkg20001 <mkg20001@gmail.com>
2016-07-28 09:55:36 +0200

    Update

--- scripts/ipfs-publish.sh
+++ scripts/ipfs-publish.sh
@@ -1,4 +1,4 @@
 ipfs add -q -r dist > ipfs-publish.log
 tail -n1 ipfs-publish.log > IPFS-HASH
-echo http://ipfs.io/ipfs/$(cat IPFS-HASH) > IPFS-URL
+echo https://ipfs.io/ipfs/$(cat IPFS-HASH) > IPFS-URL
 #ipfs name publish $(cat IPFS-HASH) > ipns-publish.log
